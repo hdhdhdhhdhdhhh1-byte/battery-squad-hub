5be9b1319c2a80f5cd4610c456c0d2a7e2c8c8a3
hdhdhdhhdhdhhh1-byte <hdhdhdhhdhdhhh1@gmail.com>
2026-07-21 02:00:18 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,6 @@
-import { createClient } from '@supabase/supabase-js' 
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl =imort.mate.env.VITE_SUPABASE_KUL
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
-const supabaseKey = import.mate.env.VITE_SUPABASE_KEY​​​​
-
-const supabase = createClient ( supabaseUrl , supabaseKey ) 
-
-export default supabase
+export const supabase = createClient(supabaseUrl, supabaseKey)
